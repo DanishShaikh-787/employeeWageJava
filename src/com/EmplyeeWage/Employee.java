ea673c805fe23470a7db5388efe5be59f06ef63f
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-20 22:50:04 +0530

    Added Welcome To Employee Wage Program

---
+++ src/com/EmplyeeWage/Employee.java
@@ -0,0 +1,9 @@
+package com.EmplyeeWage;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stud
+				System.out.println("Welcome To Employee Wage Problem");
+	}
+}
