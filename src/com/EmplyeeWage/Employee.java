b68933011c7588bec1e87b0be874eb5b9a7aa5ff
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-20 23:43:27 +0530

    Added : Eployee One Day Salary

--- src/com/EmplyeeWage/Employee.java
+++ src/com/EmplyeeWage/Employee.java
@@ -3,14 +3,17 @@ package com.EmplyeeWage;
 public class Employee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 			int IS_FULL_TIME = 1;
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 2 );
 			
 			if (empCheck == IS_FULL_TIME) {
-				System.out.println("Employee Is Present");
+				int empRatePerHours = 20;
+				int empHour = 8;
+				int salary = empRatePerHours*empHour;
+				System.out.println("One Day Salary Is : "+salary);
 			} else {
-				System.out.println("Emoployee IS Abscent");
-			}
+				int salary = 0;
+				System.out.println("One Day Salary Is : "+salary);
+			}			
 	}
 }
