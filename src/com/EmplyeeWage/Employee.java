e8fe8682f2ece08c3c4ec4d48c1d13ae2d846f26
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-20 23:00:22 +0530

    Added : Employe Attendance

--- src/com/EmplyeeWage/Employee.java
+++ src/com/EmplyeeWage/Employee.java
@@ -3,7 +3,14 @@ package com.EmplyeeWage;
 public class Employee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stud
-				System.out.println("Welcome To Employee Wage Problem");
+		// TODO Auto-generated method stub
+			int IS_FULL_TIME = 1;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 2 );
+			
+			if (empCheck == IS_FULL_TIME) {
+				System.out.println("Employee Is Present");
+			} else {
+				System.out.println("Emoployee IS Abscent");
+			}
 	}
 }
