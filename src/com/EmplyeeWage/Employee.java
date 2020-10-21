05f53f8277bba5785e2584ce94a85fbdf91694e0
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-21 22:13:19 +0530

    Added : Employee One Day Part Time Salary

--- src/com/EmplyeeWage/Employee.java
+++ src/com/EmplyeeWage/Employee.java
@@ -4,16 +4,23 @@ public class Employee {
 
 	public static void main(String[] args) {
 			int IS_FULL_TIME = 1;
-			int empCheck = (int) (Math.floor(Math.random() * 10) % 2 );
+			int IS_PART_TIME = 2;
+			int empRatePerHours = 20;
+			int Full_Time_Hour=8;
+			int Part_Time_Hour=4;
+			
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3 );
 			
 			if (empCheck == IS_FULL_TIME) {
-				int empRatePerHours = 20;
-				int empHour = 8;
-				int salary = empRatePerHours*empHour;
-				System.out.println("One Day Salary Is : "+salary);
-			} else {
-				int salary = 0;
-				System.out.println("One Day Salary Is : "+salary);
-			}			
+				int salary = empRatePerHours*Full_Time_Hour;
+				System.out.println("One Day Full Time Salary Is : "+salary);
+			}
+			
+			else if (empCheck == IS_PART_TIME) { 
+				int salary1 = empRatePerHours*Part_Time_Hour;
+				System.out.println("One Day Part Time Salary Is : "+salary1);
+			}
+				else 
+				System.out.println("Employee Is Absent");
+			}
 	}
-}
