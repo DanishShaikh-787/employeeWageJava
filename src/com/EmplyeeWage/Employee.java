f3bd7b1eeef7969e6575e7207b960dac523cec1c
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-21 22:37:54 +0530

    Added : Employee One Day Salary count Using Switch case

--- src/com/EmplyeeWage/Employee.java
+++ src/com/EmplyeeWage/Employee.java
@@ -1,26 +1,28 @@
 package com.EmplyeeWage;
 
 public class Employee {
-
+	
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	
 	public static void main(String[] args) {
-			int IS_FULL_TIME = 1;
-			int IS_PART_TIME = 2;
+		
 			int empRatePerHours = 20;
 			int Full_Time_Hour=8;
 			int Part_Time_Hour=4;
 			
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3 );
 			
-			if (empCheck == IS_FULL_TIME) {
-				int salary = empRatePerHours*Full_Time_Hour;
-				System.out.println("One Day Full Time Salary Is : "+salary);
-			}
-			
-			else if (empCheck == IS_PART_TIME) { 
-				int salary1 = empRatePerHours*Part_Time_Hour;
-				System.out.println("One Day Part Time Salary Is : "+salary1);
-			}
-				else 
-				System.out.println("Employee Is Absent");
+				switch (empCheck) { 
+				case IS_FULL_TIME :
+					System.out.println("Employee Full Time Salary : "+(empRatePerHours*Full_Time_Hour));
+					break;
+				case IS_PART_TIME :
+					System.out.println("Employee Full Time Salary : "+(empRatePerHours*Part_Time_Hour));
+					break;
+				default :
+					System.out.println("Employee IS Absent");
+					break;
+				}
 			}
 	}
