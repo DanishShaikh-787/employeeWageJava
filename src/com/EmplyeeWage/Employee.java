c68f50d655af3edff66c86f08b4682ecaa219000
Danish Shaikh <dishaikh1994@gmail.com>
2020-10-23 22:43:07 +0530

    Refactor : Employee Wage Using Class Method

--- src/com/EmplyeeWage/Employee.java
+++ src/com/EmplyeeWage/Employee.java
@@ -7,31 +7,35 @@ public class Employee {
 	public static final int empRatePerHours = 20;
 	public static final int Num_Working_Days=20;
 	public static final int Num_Working_Hours=100;
-
+	
 	public static void main(String[] args) {
-
-			int empHours=0;
-			int TotalWorkingDays=0;
-			int TotalWorkingHours=0;
-			int TotalSalary=0;
-
-			while (TotalWorkingDays < Num_Working_Days || TotalWorkingHours < Num_Working_Hours){
-				int empCheck = (int) (Math.floor(Math.random() * 10) % 3 );
-					switch (empCheck) {
-					case IS_FULL_TIME :
-						empHours=8;
-						break;
-					case IS_PART_TIME :
-						empHours=4;
-						break;
-					default :
-						empHours=0;
-						break;
-					}
+		workingHours();
+		}
+	
+	public static void workingHours() {
+		int TotalWorkingDays=0;
+		int TotalWorkingHours=0;
+		int TotalSalary=0;
+		int empHours=0;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3 );
+		switch (empCheck) {
+		case IS_FULL_TIME :
+			empHours=8;
+			break;
+		case IS_PART_TIME :
+			empHours=4;
+			break;
+		default :
+			empHours=0;
+			break;
+		}
+			while (TotalWorkingDays < Num_Working_Days && TotalWorkingHours < Num_Working_Hours){
 				TotalWorkingHours=(TotalWorkingHours+empHours);
 				TotalWorkingDays++ ;
 				}
 			TotalSalary=empRatePerHours*TotalWorkingHours;
+			System.out.println("Number Of Working Days :"+TotalWorkingDays);
+			System.out.println("Number Of Working Hours :"+TotalWorkingHours);
 			System.out.println("Total Days Salary Is : "+TotalSalary);
-			}
+	}	
 }
